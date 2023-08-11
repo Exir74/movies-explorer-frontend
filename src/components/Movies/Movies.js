c4caac8c6f522d162d1f27b78bb6b7ef6577f4a4
Exir74 <exir74@gmail.com>
2023-08-11 14:20:20 +0500

    feat: color preloader

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -2,7 +2,7 @@ import React from 'react';
 import SearchForm from './SearchForm/SearchForm';
 import MoviesCardList from './MoviesCardList/MoviesCardList';
 
-import Preloader from './Preloader/Preloader';
+import Preloader from '../Preloader/Preloader';
 
 function Movies({ isShortMovie, isShortMovieHandler }) {
   return (
