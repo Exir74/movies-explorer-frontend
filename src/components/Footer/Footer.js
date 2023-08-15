71a07ec5be96cfae741fca5b30a00c53ef68e115
Exir74 <exir74@gmail.com>
2023-08-15 15:25:24 +0500

    fix: <nav>

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,26 +10,28 @@ function Footer() {
         </p>
         <p className="footer__year">&copy; 2023</p>
         <nav className="footer__nav">
-          <li className="footer__nav-element">
-            <a
-              href={yandexPraktikum}
-              className="footer__nav-link hover"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Яндекс.Практикум
-            </a>
-          </li>
-          <li className="footer__nav-element">
-            <a
-              href={myGithub}
-              className="footer__nav-link hover"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Github
-            </a>
-          </li>
+          <ul className="footer__nav-wrapper">
+            <li className="footer__nav-element">
+              <a
+                href={yandexPraktikum}
+                className="footer__nav-link hover"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Яндекс.Практикум
+              </a>
+            </li>
+            <li className="footer__nav-element">
+              <a
+                href={myGithub}
+                className="footer__nav-link hover"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Github
+              </a>
+            </li>
+          </ul>
         </nav>
       </div>
     </footer>
--- src/components/Main/Portfolio/Portfolio.js
+++ src/components/Main/Portfolio/Portfolio.js
@@ -10,60 +10,41 @@ function Portfolio() {
             Портфолио
           </p>
           <nav className="portfolio__nav">
-            <li className="portfolio__nav-element">
-              <a
-                href={howToLearn}
-                className="portfolio__nav-link hover"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Статичный сайт
-              </a>
-              <a
-                href={howToLearn}
-                className="portfolio__nav-arrow hover"
-                target="_blank"
-                rel="noreferrer"
-              >
-                ↗
-              </a>
-            </li>
-            <li className="portfolio__nav-element">
-              <a
-                href={russianTravel}
-                className="portfolio__nav-link hover"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Адаптивный сайт
-              </a>
-              <a
-                href={russianTravel}
-                className="portfolio__nav-arrow hover"
-                target="_blank"
-                rel="noreferrer"
-              >
-                ↗
-              </a>
-            </li>
-            <li className="portfolio__nav-element">
-              <a
-                href={mesto}
-                className="portfolio__nav-link hover"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Одностраничное приложение
-              </a>
-              <a
-                href={mesto}
-                className="portfolio__nav-arrow hover"
-                target="_blank"
-                rel="noreferrer"
-              >
-                ↗
-              </a>
-            </li>
+            <ul className="portfolio__nav-wrapper">
+              <li className="portfolio__nav-element">
+                <a
+                  href={howToLearn}
+                  className="portfolio__nav-link hover"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <p className="portfolio__nav-arrow">Статичный сайт</p>
+                  <p className="portfolio__nav-arrow">↗</p>
+                </a>
+              </li>
+              <li className="portfolio__nav-element">
+                <a
+                  href={russianTravel}
+                  className="portfolio__nav-link hover"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <p className="portfolio__nav-arrow">Адаптивный сайт</p>
+                  <p className="portfolio__nav-arrow">↗</p>
+                </a>
+              </li>
+              <li className="portfolio__nav-element">
+                <a
+                  href={mesto}
+                  className="portfolio__nav-link hover"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <p className="portfolio__nav-arrow">Одностраничное приложение</p>
+                  <p className="portfolio__nav-arrow">↗</p>
+                </a>
+              </li>
+            </ul>
           </nav>
         </div>
       </div>
