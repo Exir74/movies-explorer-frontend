137b5e04107ed3ea6f8e1a22428b00f14c7fc2b4
Exir74 <exir74@gmail.com>
2023-08-15 10:34:58 +0500

    feat: register done

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 function NotFound() {
+  const navigate = useNavigate();
+  const goBack = () => {
+    navigate(-1);
+  };
   return (
     <div className="not-found">
       <h3 className="not-found__title">404</h3>
       <h4 className="not-found__description">Страница не найдена</h4>
-      <Link className="not-found__back-link" to="./">Назад</Link>
+      {/* <Link className="not-found__back-link" to={'./'}>Назад</Link> */}
+      <button
+        className="not-found__back-link"
+        type="button"
+        onClick={goBack}
+      >
+        Назад
+      </button>
     </div>
   );
 }
