dde7fbc856c41ba82e0c616b9505a84a6ad38743
Exir74 <exir74@gmail.com>
2023-08-23 17:20:25 +0500

    test: validation

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import ButtonMain from '../ButtonMain/ButtonMain';
+import useForm from '../../utils/hooks/useForm';
+import useValidation from '../../utils/hooks/useValidation';
 
 function Register({ FormHeader, logo }) {
+  const {
+    values, handleChange, resetFrom, errors, isValid,
+  } = useValidation();
+  useEffect(() => {
+    resetFrom({}, {}, true);
+  }, [resetFrom]);
   return (
     <section className="register-section">
       <FormHeader logo={logo} greeting="Добро пожаловать!" />
@@ -13,19 +21,23 @@ function Register({ FormHeader, logo }) {
           className="register__input register__input_userName"
           name="name"
           id="name-input-register"
-          // placeholder="E-mail"
           type="text"
+          pattern="[A-Za-zА-ЯЁа-яё \-]{1,}"
+          minLength={2}
+          maxLength={20}
           required
+          value={values.name || ''}
+          onChange={handleChange}
         />
-        {/* <div className="register__error-wrapper"> */}
-        {/*  <label */}
-        {/*    htmlFor="email-input-register" */}
-        {/*    className="register__input-message" */}
-        {/*    id="name-input-error" */}
-        {/*  > */}
-        {/*    Что-то пошло не так... */}
-        {/*  </label> */}
-        {/* </div> */}
+        <div className="register__error-wrapper">
+          <label
+            htmlFor="email-input-register"
+            className="register__input-message"
+            id="name-input-error"
+          >
+            {errors.name || ''}
+          </label>
+        </div>
         <span className="register__input-name">E-mail</span>
         <input
           className="register__input register__input_email"
@@ -35,27 +47,30 @@ function Register({ FormHeader, logo }) {
           type="email"
           required
           autoComplete="email"
+          value={values.email || ''}
+          onChange={handleChange}
         />
-        {/* <div className="register__error-wrapper"> */}
-        {/*  <label */}
-        {/*    htmlFor="email-input-register" */}
-        {/*    className="register__input-message" */}
-        {/*    id="name-input-error" */}
-        {/*  > */}
-        {/*    Что-то пошло не так... */}
-        {/*  </label> */}
-        {/* </div> */}
+        <div className="register__error-wrapper">
+          <label
+            htmlFor="email-input-register"
+            className="register__input-message"
+            id="name-input-error"
+          >
+            {errors.email || ''}
+          </label>
+        </div>
         <span className="register__input-name">Пароль</span>
         <input
           className="register__input register__input_password"
           name="password"
           id="password-input-register"
-          // placeholder="Пароль"
           autoComplete="current-password"
           type="password"
           required
           minLength={6}
           maxLength={10}
+          value={values.password || ''}
+          onChange={handleChange}
         />
         <div className="register__error-wrapper">
           <label
@@ -63,12 +78,12 @@ function Register({ FormHeader, logo }) {
             className="register__input-message"
             id="name-input-error"
           >
-            Что-то пошло не так...
+            {errors.password || ''}
           </label>
         </div>
         <div className="register__form-button">
           <ButtonMain
-            text="Войти"
+            text="Зарегистрироваться"
             isHide={false}
           />
         </div>
---
+++ src/utils/hooks/test.js
@@ -0,0 +1,54 @@
+// import { useEffect, useState } from 'react';
+//
+// const useValidation = (value, validations, validity) => {
+//   const [isEmpty, setIsEmpty] = useState(true);
+//   const [minLengthError, setMinLengthError] = useState(false);
+//   const [isEmailError, setIsEmailError] = useState(false);
+//   const [errorMessage, setErrorMessage] = useState('');
+//   // const re = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]
+//   // +)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
+//   const re = /^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/gim;
+//   useEffect(() => {
+//     // eslint-disable-next-line guard-for-in,no-restricted-syntax
+//     for (const validation in validations) {
+//       switch (validation) {
+//         case 'minLength':
+//           if (value.length < validations[validation]) {
+//             setMinLengthError(true);
+//           } else {
+//             setMinLengthError(false);
+//           }
+//           break;
+//         case 'isEmpty':
+//           if (value) {
+//             setIsEmpty(false);
+//           } else {
+//             setIsEmpty(true);
+//           }
+//           break;
+//         case 'isEmail':
+//           setIsEmailError(re.test(String(value)));
+//           break;
+//         default:
+//       }
+//       if (minLengthError) {
+//         setErrorMessage(`Длина дожна быть длинее ${validations.minLength} символов `);
+//       } else if (isEmpty) {
+//         setErrorMessage('Поле не может быть пустым');
+//       } else if (isEmailError) {
+//         setErrorMessage('Введите корректный email');
+//       }
+//     }
+//   }, [value]);
+//   return {
+//     isEmpty,
+//     minLengthError,
+//     isEmailError,
+//     errorMessage,
+//     validity,
+//   };
+// };
+// // export default useValidation;
+
+// const re = /^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/gim;
+// const nameReg = /^[ a-zA-Zа-яёА-ЯЁ-]+$/;
